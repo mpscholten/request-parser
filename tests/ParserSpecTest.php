9a8b69a5281b5c11de8d5b20980befa30ec108c5
Yuvi <yuval@quintly.com>
2016-08-18 09:47:59 +0200

    tests, too

--- tests/ParserSpecTest.php
+++ tests/ParserSpecTest.php
@@ -166,14 +166,14 @@ class ParserSpecTest extends \PHPUnit_Framework_TestCase
 
     public function testIntBetweenValidatorWithValuesOutOfRange()
     {
-        $this->setExpectedException(InvalidValueException::class, 'Invalid value for parameter "groupId". Expected an integer value between 1 and 6, but got "7"');
+        $this->setExpectedException(InvalidValueException::class, 'Invalid value for parameter "groupId". Expected an integer between 1 and 6, but got "7"');
         $parser = new IntParser(new Config(), 'groupId', 7);
         $groupId = $parser->between(1, 6)->required();
     }
 
     public function testFloatBetweenValidatorWithValuesOutOfRange()
     {
-        $this->setExpectedException(InvalidValueException::class, 'Invalid value for parameter "precipitation". Expected a float value between 60.99 and 101.12, but got "101.13"');
+        $this->setExpectedException(InvalidValueException::class, 'Invalid value for parameter "precipitation". Expected a float between 60.99 and 101.12, but got "101.13"');
         $parser = new FloatParser(new Config(), 'precipitation', 101.13);
         $precipitation = $parser->between(60.99, 101.12)->required();
     }
